36b22f3ca920bfcac95ad69fda39a49f50e37a60
sanjidaj <sanjidajahan341@gmail.com>
2026-03-31 18:11:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-red-600 text-center bg-green-300 '>Hello</h2>
+      <button className='btn btn-primary'>test</button>
+      
+    </>
+  )
+}
+
+export default App
